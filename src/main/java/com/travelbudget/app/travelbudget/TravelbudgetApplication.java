c23e482bae610c1ad765da58ecdd356584de6a2e
Mohammed JOUHRI <10jouhri@gmail.com>
2021-05-03 22:30:15 +0200

    first commit

---
+++ src/main/java/com/travelbudget/app/travelbudget/TravelbudgetApplication.java
@@ -0,0 +1,26 @@
+package com.travelbudget.app.travelbudget;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class TravelbudgetApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelbudgetApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO trip (idtrip, name) VALUES(?,?)";
+		int result = jdbcTemplate.update(sql, "1","bali");
+
+		if (result > 0) System.out.println("A new row has been inserted");
+	}
+}
---
+++ src/test/java/com/travelbudget/app/travelbudget/TravelbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbudget.app.travelbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
